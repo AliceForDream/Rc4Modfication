efadea7ebdcf64cd3848d45e576ff6b9dde0b8d2
AliceForDream <80669969+AliceForDream@users.noreply.github.com>
2021-10-29 08:09:18 +0800

    Update StdAfx.h

--- StdAfx.h
+++ StdAfx.h
@@ -1,10 +1,5 @@
-// stdafx.h : include file for standard system include files,
-//  or project specific include files that are used frequently, but
-//      are changed infrequently
-//
-
-#if !defined(AFX_STDAFX_H__D71FE4C0_81B3_8B26_09C0_2F068C674A3D__INCLUDED_)
-#define AFX_STDAFX_H__D71FE4C0_81B3_8B26_09C0_2F068C674A3D__INCLUDED_
+#if !defined(RC4MCRYPT_5007af035c665c06e479b975573bbfbd9a4442fc__INCLUDED_)
+#define RC4MCRYPT_5007af035c665c06e479b975573bbfbd9a4442fc__INCLUDED_
 
 #if _MSC_VER > 1000
 #pragma once
@@ -14,10 +9,5 @@
 
 #include <stdio.h>
 
-// TODO: reference additional headers your program requires here
-
-//{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
-#endif // !defined(AFX_STDAFX_H__D71FE4C0_81B3_8B26_09C0_2F068C674A3D__INCLUDED_)
+#endif // !defined(RC4MCRYPT_5007af035c665c06e479b975573bbfbd9a4442fc__INCLUDED_)
 
